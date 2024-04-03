345c36526b6c656953f913f4b5e3021dd812e09f
Benni-Math <benediktjens.arnarsson@gmail.com>
2024-04-03 03:32:19 -0400

    Add yargs and tsup, start src folder

---
+++ src/cli.ts
@@ -0,0 +1,16 @@
+// Import NodeJS libs
+// import { execSync } from 'child_process';
+
+// Import NPM libs
+// import chalk from 'chalk';
+// import { Command } from 'commander';
+// import figlet from 'figlet';
+// import moment from 'moment';
+// import { table } from 'table';
+// import tempy from 'tempy';
+// import untildify from 'untildify';
+// import yn from 'yn';
+
+// Import AWS helpers
+
+// Import configuration
